7d697b8e4c7ae95d58dc9c7e2cf1a820f29675ce
wski <contact.mattdylan@gmail.com>
2017-02-25 18:24:01 -0500

    comments and stuff

--- core.js
+++ core.js
@@ -5,6 +5,7 @@ let theirBlockKey = ''
 
 const chatBox = document.getElementById('chat')
 
+// Adds a local message
 const addMyMessage = function(msg, sub) {
   if (sub) {
     chatBox.innerHTML += `<p class="nopad" style="color: #95a5a6;">${msg}</p>`
@@ -13,38 +14,54 @@ const addMyMessage = function(msg, sub) {
   }
 }
 
+// Adds a message to the chatbox
 const sendMessage = function(msg) {
   const decrypted = CryptoJS.AES.decrypt(msg, theirBlockKey.replace(/\s/g, ''))
 
   if (decrypted.toString(CryptoJS.enc.Utf8)) {
     chatBox.innerHTML += `<p class="nopad" style="color: #34495e;"><span class="timestamp">[${Date.now()}]:</span> ${decrypted.toString(CryptoJS.enc.Utf8)}</p>`
   } else {
-    chatBox.innerHTML += `<p class="nopad" style="color: #34495e;"><span class="timestamp">[${Date.now()}]:</span> Failed to decrypt message, need blockkey to decrypted.</p>`
+    chatBox.innerHTML += `<p class="nopad" style="color: #34495e;"><span class="timestamp">[${Date.now()}]:</span> ${msg}</p>`
   }
 
+  // Automatically scroll to bottom of div
   chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 // P2P
 const peer = new Peer({key: 't6ll478mrrv34n29'})
 
+// Once we've gotten a peer ID from the broker, let us know what it is so we may share it
 peer.on('open', (id) => {
   document.getElementById("peerID").innerText = id 
 })
 
 peer.on('connection', function (conn) {
+  // ID of peer connection to us
   const pid = conn.peer
-  chatBox.innerHTML += `<p><span class="timestamp">New connection from peer: ${pid}</p>`
+  
   conn.on('open', function() {
+    // Let us know we've made a connection
+    chatBox.innerHTML += `<p><span class="timestamp">New connection from peer: ${pid}</p>`
+
     conn.on('data', function(data) {
       sendMessage(data)
     })
     document.getElementById('send').onclick = function() {
+      // Get message and clear input
       const val = document.getElementById('message').value
       document.getElementById('message').value = ''
+
+      // Send message to chatbox unencrypted so we remember what we've said
       addMyMessage(val)
+
+      // Encrypt the message
       const msg = CryptoJS.AES.encrypt(val, key.replace(/\s/g, ''))
+
+      // Add the encrypted version just for fun
       addMyMessage(msg.toString(), true)
+
+      // Send the encrypted message to the peer
       conn.send(msg.toString())
     }
   })
@@ -56,11 +73,15 @@ document.getElementById('connect').onclick = function() {
   const theirID = document.getElementById('theirPeerID').value
   const theirBlock = document.getElementById('theirBlockKey').value
 
+  // Update the global block key used to decrypt
   theirBlockKey = theirBlock
 
+  // Connect to a new peer
   const client = peer.connect(theirID);
 
+  // When the connection is open, add any messages to the chatbox
   client.on('open', function() {
+    // Let us know we've connected successfuly
     addMyMessage(`Connected to ${theirID}!`)
 
     client.on('data', function(data) {
